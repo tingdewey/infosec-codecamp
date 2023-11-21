6a1841a9eabcbb59c19a4814a2d69230a5415bf4
tingdewey <petermbrowne@hotmail.com>
2023-11-21 17:00:47 +0000

    contentsecpolicy added

--- myApp.js
+++ myApp.js
@@ -12,7 +12,13 @@ app.use(helmet.hsts({maxAge: timeInSeconds, force: true}))
 app.use(helmet.dnsPrefetchControl())
 app.use(helmet.noCache())
 
-
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+  defaultSrc: ["'self'"],
+  scriptSrc: ["'self'", 'trusted-cdn.com'],
+}
+})
+);
 
 
 
