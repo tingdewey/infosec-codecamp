ef9b12acc9385b8b0f12f859e1845ed3131365ae
tingdewey <petermbrowne@hotmail.com>
2023-11-21 16:23:07 +0000

    xxsfilter added

--- myApp.js
+++ myApp.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const helmet = require('helmet');
-
 const app = express();
+
 app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({action: 'deny'}));
-
+app.use(helmet.xssFilter())
 
 
 
